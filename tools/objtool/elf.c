a07a45886820c28ab202962a33816951e4e60e57
zy-nscc ubuntu20.04 desktop <1920548152@qq.com>
2023-02-28 19:06:23 +0800

    修改了elf.c和添加source文件

--- tools/objtool/elf.c
+++ tools/objtool/elf.c
@@ -224,8 +224,7 @@ static int read_symbols(struct elf *elf)
 
 	symtab = find_section_by_name(elf, ".symtab");
 	if (!symtab) {
-		WARN("missing symbol table");
-		return -1;
+		return 0;
 	}
 
 	symbols_nr = symtab->sh.sh_size / symtab->sh.sh_entsize;
