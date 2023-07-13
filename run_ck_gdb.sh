399c881125c77cd3fde40f17a1b416821854de67
Ziyan ZHANG <zhangzy273@mail2.sysu.edu.cn>
2023-07-13 11:36:20 +0800

    gdb脚本命名优化

--- run_ck_gdb.sh
+++ run_ck_gdb.sh
@@ -4,4 +4,5 @@ qemu-system-x86_64 -s -S\
 	-hda ~/lab/ubuntu.img \
 	-append "root=/dev/sda2 rw console=ttyS0" \
 	-hdb ~/lab/swap_ext4.img \
-	-hdc ~/lab/exe_mj.img
+	-hdc ~/lab/exe_mj.img \
+	-hdd ~/lab/vana_ext2.img 
--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -1,1 +1,4 @@
-qemu-system-x86_64 -m 2048 --enable-kvm ~/lab/ubuntu.img
+qemu-system-x86_64 -m 2048 --enable-kvm \
+	-nographic \
+	-serial mon:stdio \
+
